1e874d75330b8904602b892477afa47bb7d2a9ea
PalomaSa81 <palomasa.art@gmail.com>
2026-04-12 02:59:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,25 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
+// Essas infos têm que ser IDÊNTICAS às do seu index.html
+const firebaseConfig = {
   apiKey: "AIzaSyDtMO8dbJWXcIdmhCAtvYdxKlntulcYqBI",
   projectId: "dahj-hub",
   messagingSenderId: "964532263352",
   appId: "1:964532263352:web:fe3d32318ed86be611d74d"
-});
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Lógica para exibir notificação em segundo plano
+// Isso aqui é o que faz a mágica da notificação aparecer com o Chrome fechado
 messaging.onBackgroundMessage((payload) => {
-  console.log('Notificação em background:', payload);
+  console.log('[sw.js] Recebida mensagem em background: ', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: 'logo-dahj.jpg'
+    icon: 'logo-dahj.jpg', // Certifique-se que esse arquivo existe na raiz
+    badge: 'logo-dahj.jpg'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
