f9d07eba3989530f3ccafbb46437b426c58b8427
PalomaSa81 <palomasa.art@gmail.com>
2026-04-12 02:30:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,32 +2,22 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDtMO8dbJWXcIdmhCAtvYdxKlntulcYqBI",
-    projectId: "dahj-hub",
-    messagingSenderId: "964532263352",
-    appId: "1:964532263352:web:fe3d32318ed86be611d74d"
+  apiKey: "AIzaSyDtMO8dbJWXcIdmhCAtvYdxKlntulcYqBI",
+  projectId: "dahj-hub",
+  messagingSenderId: "964532263352",
+  appId: "1:964532263352:web:fe3d32318ed86be611d74d"
 });
 
 const messaging = firebase.messaging();
 
-// GESTOR DE SEGUNDO PLANO
+// Lógica para exibir notificação em segundo plano
 messaging.onBackgroundMessage((payload) => {
-    // Se a notificação já veio pronta (como configuramos no Apps Script),
-    // o navegador vai usar a TAG para unificar.
-    console.log("Notificação recebida:", payload);
-});
+  console.log('Notificação em background:', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'logo-dahj.jpg'
+  };
 
-self.addEventListener('notificationclick', (event) => {
-    event.notification.close();
-    const urlParaAbrir = 'https://palomasa81.github.io/o_eco_prototipo/';
-
-    event.waitUntil(
-        clients.matchAll({ type: 'window', includeUncontrolled: true })
-            .then((windowClients) => {
-                for (let client of windowClients) {
-                    if (client.url === urlParaAbrir && 'focus' in client) return client.focus();
-                }
-                if (clients.openWindow) return clients.openWindow(urlParaAbrir);
-            })
-    );
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
